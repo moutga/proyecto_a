006d753c90bd9eee5089c83f828cda9ee020f83e
moutga <moutga@gmail.com>
2021-06-01 17:25:05 -0300

    Boton listo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <div>
+      <Boton spanX="2" spanY="1" texto="Soy un botón" />
+    </div>
+
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+//import HelloWorld from '@/components/HelloWorld.vue'
+import Boton from '@/components/Boton.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    //HelloWorld,
+    Boton
   }
 }
 </script>
